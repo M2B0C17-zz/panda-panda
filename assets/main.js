8632bf542b4f492fc6ba9c315e963eb50562ef42
macarena baltra <macarena.bc2011@gmail.com>
2017-05-21 15:55:41 -0400

    Subiendo intento 1 de evento X

--- assets/main.js
+++ assets/main.js
@@ -3,9 +3,23 @@
 // Al apretar en la X, la imagen debe eliminarse
 
 // Seleccion de foto
-var fotos = document.getElementsByClass("eventX").value;
+function fotosX(){
+  var fotos = document.getElementsByClass('eventX').value = "";
 
-var boton = document.getElementById('demo');
-boton.addEventListener('click', function(){
+  // Se crea el enlace para borrar
+  var paraBorrar = document.createElement('a');
+  paraBorrar.setAttribute('class', 'eventX');
 
-});
+  // Evento
+  var enlace = document.getElementsByTagName('a').addEventListener('click', function(){
+
+  //Si no se esta en el enlace tenemos la alerta que informa como eliminar
+    if(! enlace){
+      alert("Si desea eliminar la imagen, debe apretar la X");
+    }else {
+  // se crea una variable con el padre del node para luego llamar a su hijo que borrar las fotos
+        var elim = fotos.parentNode;
+		    elim.removeChild(fotos);
+    }
+  }
+}
