8a88cc7666dfcddcda86d3345a1cda886788b967
macarena baltra <macarena.bc2011@gmail.com>
2017-05-21 14:47:40 -0400

    Subiendo primeros codigos en js, para generar evento X

---
+++ assets/main.js
@@ -0,0 +1,11 @@
+// Elementos de eventos con onclick
+
+// Al apretar en la X, la imagen debe eliminarse
+
+// Seleccion de foto
+var fotos = document.getElementsByClass("eventX").value;
+
+var boton = document.getElementById('demo');
+boton.addEventListener('click', function(){
+
+});
